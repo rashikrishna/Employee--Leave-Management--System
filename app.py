f0972af8bf3fadb476e30b0aca33d087773de70b
rashi <rashikrishna16@gmail.com>
2019-10-22 16:58:30 +0530

    Updated 0n 21

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, session, render_template,request, url_for,redirect, jso
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from itsdangerous import URLSafeTimedSerializer
+import smtplib
 
 app = Flask(__name__)
 
@@ -11,13 +13,34 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 # Set up database
 engine = create_engine("postgres://tlkbyctryyffkn:b0ab12cc98ffab444f5d1d07329271b38c4e253f6cf3d1368bed287623e3dda8@ec2-54-235-86-101.compute-1.amazonaws.com:5432/d4bmrsh5lptv18")
 db = scoped_session(sessionmaker(bind=engine))
 
 notes = []
 #notes.append(note)
+# Python code to illustrate Sending mail from
+# your Gmail account
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login("software.engwkze@gmail.com", "softwareengineering@123")
+
+# message to be sent
+message = "Message_you_need_to_send"
+
+# sending the mail
+s.sendmail("rashikrishna16@gmail.com", "arashee.2000@gmail.com", message)
+
+# terminating the session
+s.quit()
+
+
 @app.route("/register", methods=["POST","GET"])
 def register():
     if request.method == "POST" :
@@ -61,7 +84,8 @@ def index():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    message = ("")
+    return render_template("login.html",message=message)
 
 @app.route("/home",methods=["POST"])
 def home():
@@ -72,7 +96,7 @@ def home():
     if category == "admin" :
         if (db.execute("SELECT * FROM admin WHERE userid=:username AND password=:password",{"username":username, "password":password}).fetchone() is None):
             message = ("Incorrect Username or Password Admin")
-            return render_template("index.html",message=message)
+            return render_template("login.html",message=message)
         else :
             session["logged_user"]=username
             lists= db.execute("SELECT * FROM faculty_leave WHERE approved = 0").fetchall()
@@ -83,17 +107,22 @@ def home():
         query = db.execute("SELECT * FROM faculty WHERE userid=:username AND password=:password",{"username":username, "password":password}).fetchone()
         if (query is None):
             message = ("Incorrect Username or Password faculty")
-            return render_template("index.html",message=message)
+            return render_template("login.html",message=message)
         else :
             session["logged_user"]=username
             id = query.id
             info = db.execute("SELECT * FROM faculty_info WHERE id=:id",{"id":id}).fetchone()
-            return render_template("home.html",userdetail = query,userinfo=info)
+            leave = db.execute("SELECT * FROM faculty_leave WHERE id=:id",{"id":id}).fetchone()
+            if (leave is None) :
+                leaveapplied = 0;
+            else :
+                leaveapplied = 1;
+            return render_template("home.html",userdetail = query,userinfo=info,leaveapplied=leaveapplied)
 
     else :
         if (db.execute("SELECT * FROM others WHERE userid=:username AND password=:password",{"username":username, "password":password}).fetchone() is None):
             message = ("Incorrect Username or Password Others")
-            return render_template("index.html",message=message)
+            return render_template("login.html",message=message)
         else :
             session["logged_user"]=username
             return render_template("home.html")
